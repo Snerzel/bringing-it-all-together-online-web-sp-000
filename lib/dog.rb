c1336cd60aff334f57f4a155b6bbc0cf46b74078
Cherissa Woudenberg <snerzel@comcast.net>
2021-06-30 20:37:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,8 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(attributes)
-    #id: nil, name:, breed:
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(attrs)
+    attrs.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
 
